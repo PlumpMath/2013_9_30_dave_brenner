8d8c0dbeee22ba5a0bd862e4479250bccefaf695
William Stein <williamstein92@gmail.com>
2013-12-05 13:16:51 -0500

    Yo

--- app/views/lesson.blade.php
+++ app/views/lesson.blade.php
@@ -5,12 +5,9 @@
 @include('modules.hud', ['title' => 'Lesson', 'icon' => 'edit', 'user_name' => $user_name])
 @include('modules.banner', ['title' => 'Lesson'])
 <div class="box bg-1">
-	<div class="box-2">
-		@include('modules.section_divide', ['name' => 'Progress'])
-	</div>
-	<div class="box-4 bg-0">
+	<div class="box measure bg-0 border-left border-bottom-0">
 		<div class="box box-pad-0 fg-1 border-0">Calendar</div>
-		<div class="box box-pad-0">
+		<div class="box box-pad-0 center">
 			@include('modules.calendar', ['calendar' => $calendar])
 		</div>
 	</div>
