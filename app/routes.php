ae703762196d5abe4e72ea83903f9d11b74b68f9
William Stein <williamstein92@gmail.com>
2013-12-10 15:14:36 -0500

    Yo

--- app/routes.php
+++ app/routes.php
@@ -925,7 +925,7 @@ Route::post('/reset/password/verify', function ()
 		return App::abort(401, 'You are not authorized.');
 
 	if ($validator->passes()) {
-		Auth::user()->password = $data['password'];
+		Auth::user()->password = Hash::make($data['password']);
 		Auth::user()->save();
 
 		$data = [
