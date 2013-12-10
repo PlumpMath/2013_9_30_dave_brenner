6ceee929e5d127369a761c60531ab0a5d8ce7d28
William Stein <williamstein92@gmail.com>
2013-12-10 16:12:31 -0500

    Yo

--- app/views/activate.blade.php
+++ app/views/activate.blade.php
@@ -3,7 +3,7 @@
 @section('content')
 @include('modules.header')
 @include('modules.hud', ['title' => 'Home', 'icon' => 'home', 'user_name' => $user_name])
-@include('modules.banner', ['title' => 'Account Activated!'])
+@include('modules.banner', ['title' => 'Account Verified!'])
 <div class="box box-pad-0 measure center">Hi {{{ $user_name }}}</div>
 <div class="box box-pad-0 measure center">OK, your account is now verified. If you click on the link below, you can continue where you left off with your registration. Thanks!</div>
 <div class="box box-pad-0 measure center"><a href="{{ $home }}">Continue to register your children</a></a></div>
