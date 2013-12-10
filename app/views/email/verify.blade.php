80b5c6733d213053e1d07dd5d613a3fc95923215
William Stein <williamstein92@gmail.com>
2013-12-10 16:17:35 -0500

    Yo

--- app/views/email/verify.blade.php
+++ app/views/email/verify.blade.php
@@ -6,4 +6,7 @@
 This is your verification email.  To verify your account with us, just click the link below.  Easy right? </h3>
 Btw: if nothing happens when you click the link, please copy and paste the link into the address bar of your browser and press enter on your keyboard. This should take you to our account activation page. If you still have trouble, call us at 631-776-8242 so we can help you. Thanks!
 <a href="{{ $link }}">Click to activate your account</a>
+
+<h3 style="display: block; font-family: 'Open Sans'; font-size: 16px; font-style: italic; font-weight: normal; line-height: 100%; letter-spacing: normal; margin: 0px 0px 10px; text-align: left; color: rgb(149, 149, 149);">This email should have gone to your inbox.</h3>
+To maximize the benefits of our website and your involvement in any programs, this email needed to land in your inbox. If it landed in your spam/junk mail folder, add help@myafterschoolprograms.com to the list of safe senders in your mail settings.
 @stop
--- app/views/verify.blade.php
+++ app/views/verify.blade.php
@@ -2,8 +2,8 @@
 
 @section('content')
 @include('modules.header')
-@include('modules.hud', ['title' => 'Verify', 'icon' => 'home', 'user_name' => $user_name])
-@include('modules.banner', ['title' => 'Verify'])
-<div class="box box-pad-0 measure center">For your security, we now need to verify your account. An email has been sent to {{{ $email }}}. Please open your email and follow the directions.  After this quick verification process, we can continue with your registration. Thanks!</div>
-<div class="box box-pad-0 measure center">Would you like us to <a href="{{ $another_email }}">send another?</a></div>
+@include('modules.hud', ['title' => 'Verify Your Account', 'icon' => 'home', 'user_name' => $user_name])
+@include('modules.banner', ['title' => 'Verify Your Account'])
+<div class="box box-pad-0 measure center">For your security, we have sent an email to {{{ $email }}}.<br/> Please open your email and follow the simple directions to complete your verification.<br/>Then, we can continue with your registration. Thanks!</div>
+<div class="box box-pad-0 measure center">IMPORTANT<br/>If you do not receive our verification email, <a href="{{ $another_email }}">click here</a> to have another sent.<br/>If you still do not receive an email, look in your spam/Junk mail folder.</div>
 @stop
