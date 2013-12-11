7d4814384109e593b2596917e5670c0447434df2
William Stein <williamstein92@gmail.com>
2013-12-11 16:40:04 -0500

    Yo

--- app/config/mail.php
+++ app/config/mail.php
@@ -3,36 +3,9 @@
 return array(
 	'driver' => 'smtp',
 	'host' => 'smtp.sendgrid.net',
-	'port' => 25,
+	'port' => 26,
 	'from' => array('address' => 'help@myafterschoolprograms.com', 'name' => 'myafterschoolprograms'),
 	'encryption' => 'tls',
-	'username' => 'sendgrid_username',
+	'username' => 'davebrenner',
 	'password' => 'sendgrid_password',
-
-	/*
-	|--------------------------------------------------------------------------
-	| Sendmail System Path
-	|--------------------------------------------------------------------------
-	|
-	| When using the "sendmail" driver to send e-mails, we will need to know
-	| the path to where Sendmail lives on this server. A default path has
-	| been provided here, which will work well on most of your systems.
-	|
-	*/
-
-	'sendmail' => '/usr/sbin/sendmail -bs',
-
-	/*
-	|--------------------------------------------------------------------------
-	| Mail "Pretend"
-	|--------------------------------------------------------------------------
-	|
-	| When this option is enabled, e-mail will not actually be sent over the
-	| web and will instead be written to your application's logs files so
-	| you may inspect the message. This is great for local development.
-	|
-	*/
-
-	'pretend' => false,
-
 );
